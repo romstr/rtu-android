ac5835c294847d97bad379e600e238d6767b4084
Romans Strancevskis <romans.strancevskis@citadele.lv>
2020-08-30 18:08:58 +0300

    RecyclerView (Practice04)

--- app/src/main/java/lv/romstr/mobile/rtu_android/MainActivity.kt
+++ app/src/main/java/lv/romstr/mobile/rtu_android/MainActivity.kt
@@ -2,10 +2,32 @@ package lv.romstr.mobile.rtu_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = mutableListOf(
+            ShoppingItem("Bread", 1, "pcs."),
+            ShoppingItem("Eggs", 10, "pcs."),
+            ShoppingItem("Milk", 1, "l"),
+            ShoppingItem("Potatoes", 2, "kg"))
+
+    private lateinit var adapter: ShoppingItemRecyclerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ShoppingItemRecyclerAdapter(items)
+        mainItems.adapter = adapter
+
+        mainButtonAdd.setOnClickListener { appendItem() }
+    }
+
+    private fun appendItem() {
+        items.add(ShoppingItem(mainEditName.text.toString(), 1, ""))
+        items.sortBy { it.name }
+        mainEditName.text.clear()
+        adapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/lv/romstr/mobile/rtu_android/ShoppingItem.kt
@@ -0,0 +1,7 @@
+package lv.romstr.mobile.rtu_android
+
+data class ShoppingItem(
+    val name: String,
+    val quantity: Int,
+    val unit: String
+)
