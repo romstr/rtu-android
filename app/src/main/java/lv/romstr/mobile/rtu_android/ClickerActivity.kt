3722ad763571f15cf433974964eb93e1ebb48bcf
Romans Strancevskis <romans.strancevskis@citadele.lv>
2020-09-06 21:42:23 +0300

    Saving state (Practice07)

--- app/src/main/java/lv/romstr/mobile/rtu_android/ClickerActivity.kt
+++ app/src/main/java/lv/romstr/mobile/rtu_android/ClickerActivity.kt
@@ -1,8 +1,8 @@
 package lv.romstr.mobile.rtu_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_clicker.*
 
 class ClickerActivity : AppCompatActivity() {
@@ -13,7 +13,15 @@ class ClickerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clicker)
 
+        clicks = savedInstanceState?.getInt(CLICKS_EXTRA) ?: 0
+        clickerText.text = "$clicks"
+
         clickerButton.setOnClickListener { incrementClickCount() }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(CLICKS_EXTRA, clicks)
     }
 
     private fun incrementClickCount() {
@@ -24,5 +32,9 @@ class ClickerActivity : AppCompatActivity() {
             Toast.makeText(this, getString(R.string.clicker_toast, clicks), Toast.LENGTH_SHORT)
                 .show()
         }
+    }
+
+    companion object {
+        private const val CLICKS_EXTRA = "lv.romstr.mobile.clicks"
     }
 }
