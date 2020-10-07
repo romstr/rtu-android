cb297e39449082f16b78a61dc07c2e671bbb6637
andrii koniushenko <andrii.koniushenko@citadele.lv>
2020-10-07 16:38:22 +0300

    Init ViewModel in Activity

--- app/src/main/java/lv/romstr/mobile/rtu_android/clicker/ClickerActivity.kt
+++ app/src/main/java/lv/romstr/mobile/rtu_android/clicker/ClickerActivity.kt
@@ -3,35 +3,35 @@ package lv.romstr.mobile.rtu_android.clicker
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_clicker.*
 import lv.romstr.mobile.rtu_android.R
 
 class ClickerActivity : AppCompatActivity() {
 
-    private var clicks = 0
+    private lateinit var viewModel: ClickerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clicker)
 
-        clicks = savedInstanceState?.getInt(CLICKS_EXTRA) ?: 0
-        clickerText.text = "$clicks"
+        viewModel = ViewModelProvider(this).get(ClickerViewModel::class.java)
+        viewModel.clicks = savedInstanceState?.getInt(CLICKS_EXTRA) ?: 0
+        clickerText.text = "${viewModel.clicks}"
 
         clickerButton.setOnClickListener { incrementClickCount() }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(CLICKS_EXTRA, clicks)
-    }
-
     private fun incrementClickCount() {
-        clicks++
-        clickerText.text = "$clicks"
+        viewModel.clicks++
+        clickerText.text = "${viewModel.clicks}"
 
-        if (clicks % 10 == 0) {
-            Toast.makeText(this, getString(R.string.clicker_toast, clicks), Toast.LENGTH_SHORT)
-                .show()
+        if (viewModel.clicks % 10 == 0) {
+            Toast.makeText(
+                this,
+                getString(R.string.clicker_toast, viewModel.clicks),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
--- app/src/main/java/lv/romstr/mobile/rtu_android/clicker/ClickerViewModel.kt
+++ app/src/main/java/lv/romstr/mobile/rtu_android/clicker/ClickerViewModel.kt
@@ -2,4 +2,8 @@ package lv.romstr.mobile.rtu_android.clicker
 
 import androidx.lifecycle.ViewModel
 
-class ClickerViewModel : ViewModel()
+class ClickerViewModel : ViewModel() {
+
+    var clicks = 0
+
+}
